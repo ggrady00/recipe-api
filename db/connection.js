d70e91c1973f6915e5e1b79123a33e72d2532234
George Grady <george.grady@hotmail.com>
2025-09-15 00:00:03 +0100

    connection fix

--- db/connection.js
+++ db/connection.js
@@ -10,6 +10,8 @@ const config = {}
 if (ENV === 'production') {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
+    config.ssl = {rejectUnauthorized: false};
+    config.family = 4;
 }
 console.log(config)
 
